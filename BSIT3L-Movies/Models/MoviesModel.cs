5b175709b596553b1c79890fffefb893c4b4dbc8
enlowjap <145821911+enlowjap@users.noreply.github.com>
2023-10-14 07:55:30 +0800

    CSS and additional
    i do edit  from controller , model, view

--- BSIT3L-Movies/Models/MoviesModel.cs
+++ BSIT3L-Movies/Models/MoviesModel.cs
@@ -9,7 +9,9 @@ namespace BSIT3L_Movies.Models
         public string Rating { get; set; }
         public int ReleaseYear { get; set; }
         public string Genre { get; set; }
-        
+        public string Trailer { get; set; }
+        public string Image { get; set; }
+        public string About { get; set; }
     }
 }
 
